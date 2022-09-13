80afa6dde9fdf5665ee6467a0d8d9e75a222fd6a
Douglas Machado <douglas-mad@live.com>
2022-09-13 16:20:35 -0300

    Create new routes and annotations

--- src/main/java/com/toolsChangelle/controller/TransactionController.java
+++ src/main/java/com/toolsChangelle/controller/TransactionController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.toolsChangelle.entities.Transaction;
 import com.toolsChangelle.services.TransactionService;
 
@@ -20,18 +21,20 @@ public class TransactionController {
 	private TransactionService service;
 
 	@GetMapping
+	@ResponseBody
 	public ResponseEntity<List<Transaction>> findAllTransactions() {
 		List<Transaction> list = service.getAll();
 		return ResponseEntity.ok(list);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Transaction> getTransactionByID(Long id) {
-		Transaction transaction = service.getByID(id);
-		return ResponseEntity.ok(transaction);
+	@ResponseBody
+	public ResponseEntity<Transaction> getTransactionByID(@PathVariable Long id) {
+		return ResponseEntity.ok(service.getByID(id));
 	}
 
 	@PostMapping
+	@ResponseBody
 	public ResponseEntity<Transaction> saveTransaction(@RequestBody Transaction transaction) {
 		System.out.println(transaction.toString());
 		return ResponseEntity.ok(service.save(transaction));
