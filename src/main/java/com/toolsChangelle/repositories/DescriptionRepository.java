e6bc41f897e62d9aeea13f6d84ac8240692f87c2
Douglas Machado <douglas-mad@live.com>
2022-09-13 14:45:55 -0300

    no file

--- src/main/java/com/toolsChangelle/repositories/DescriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.toolsChangelle.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.toolsChangelle.entities.Description;
-@Repository
-public interface DescriptionRepository extends JpaRepository<Description, Long> {}
