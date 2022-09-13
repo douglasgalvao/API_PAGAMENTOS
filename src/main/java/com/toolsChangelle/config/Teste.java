a04619883a559889f279df7e95be6d70a702e851
Douglas Machado <douglas-mad@live.com>
2022-09-13 14:46:55 -0300

    test

---
+++ src/main/java/com/toolsChangelle/config/Teste.java
@@ -0,0 +1,14 @@
+package com.toolsChangelle.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class Teste implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
+
