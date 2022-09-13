ea1180b27631b352450cd749475de782f148009b
Douglas Machado <douglas-mad@live.com>
2022-09-13 05:38:04 -0300

    First Commit, Initial Stater Project

---
+++ src/test/java/com/toolsChangelle/ToolsChangelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsChangelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChangelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
