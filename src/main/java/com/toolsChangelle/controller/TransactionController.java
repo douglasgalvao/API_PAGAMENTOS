f9e4ca06acd148dddfe30460e68733e45c527df0
Douglas Machado <douglas-mad@live.com>
2022-09-14 07:59:23 -0300

    Create new route to change refunded status transaction

--- src/main/java/com/toolsChangelle/controller/TransactionController.java
+++ src/main/java/com/toolsChangelle/controller/TransactionController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.toolsChangelle.entities.Transaction;
 import com.toolsChangelle.services.TransactionService;
 
@@ -30,6 +32,7 @@ public class TransactionController {
 	@GetMapping(value = "/{id}")
 	@ResponseBody
 	public ResponseEntity<Transaction> getTransactionByID(@PathVariable Long id) {
+		System.out.println("id========="+id);
 		return ResponseEntity.ok(service.getByID(id));
 	}
 
@@ -38,6 +41,13 @@ public class TransactionController {
 	public ResponseEntity<Transaction> saveTransaction(@RequestBody Transaction transaction) {
 		System.out.println(transaction.toString());
 		return ResponseEntity.ok(service.save(transaction));
+	}
+	
+	@PatchMapping(value= "/refunded/{transactionId}")
+	@ResponseBody
+	public ResponseEntity<Transaction> saveRefunded(@PathVariable Long transactionId) {
+		
+		return ResponseEntity.ok(service.changeExtorno(transactionId));
 	}
 
 }
