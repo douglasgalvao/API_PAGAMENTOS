bce2c46054ee812a77d25428b47f2729d350df27
Douglas Machado <douglas-mad@live.com>
2022-09-16 11:18:51 -0300

    Delete transactions reference

--- src/main/java/com/toolsChangelle/entities/Account.java
+++ src/main/java/com/toolsChangelle/entities/Account.java
@@ -1,14 +1,16 @@
 package com.toolsChangelle.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import com.toolsChangelle.Dtos.TransactionDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -19,5 +21,6 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String name;
 	private Double balance;
 }
