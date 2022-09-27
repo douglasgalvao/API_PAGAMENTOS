50a204e4c74e41776316736039e895f6dd80a4a2
Douglas Machado <douglas-mad@live.com>
2022-09-27 20:51:41 -0300

    Resolved variables path

--- src/main/java/com/toolsChangelle/entities/Account.java
+++ src/main/java/com/toolsChangelle/entities/Account.java
@@ -1,53 +1,31 @@
 package com.toolsChangelle.entities;
 
+import javax.persistence.*;
+
 import com.toolsChangelle.config.GenerateUUID;
 import lombok.*;
-import org.hibernate.Hibernate;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-import java.util.Objects;
 import java.util.UUID;
 
 
 @Entity
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class Account {
     @Id
     @GeneratedValue(generator = "uuid2", strategy = GenerationType.IDENTITY)
     @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(columnDefinition = "uuid",name = "account_id")
+    @Column(columnDefinition = "uuid")
     private UUID id;
-    @Column(name = "account_balance")
     private Double balance;
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private UserBank user;
-    @Column(name = "user_id")
     private UUID userID;
 
-    public Account(Double balance,UUID userID){
+    public Account(Double balance, UUID userID) {
         this.id = GenerateUUID.generateUuid();
-        this.userID=userID;
+        this.userID = userID;
         this.balance = balance;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Account account = (Account) o;
-        return id != null && Objects.equals(id, account.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
