0bd7e262d5774f1bf5eb3bbe1c11673c302dc8a3
Douglas Machado <douglas-mad@live.com>
2022-09-29 20:37:04 -0300

    Delete exceptions not used

---
+++ src/test/java/com/toolsChangelle/tests/services/TestePayment.java
@@ -0,0 +1,11 @@
+package com.toolsChangelle.tests.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestePayment {
+    @Test
+    public void teste() {
+        Assert.assertEquals(10, 10);
+    }
+}
