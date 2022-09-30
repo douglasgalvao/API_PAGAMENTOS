39dd9e1012dea8ac30dd66f0a238dfb6d646f5f2
Douglas Machado <douglas-mad@live.com>
2022-09-30 06:19:34 -0300

    Add Unit and Integration tests

--- src/test/java/com/Payment_API/repositories/UserRepositoryTests.java
+++ src/test/java/com/Payment_API/repositories/UserRepositoryTests.java
@@ -1,2 +1,58 @@
-package com.Payment_API.repositories;public class UserRepositoryTests {
+package com.Payment_API.repositories;
+
+import com.Payment_API.entities.UserBank;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+import java.util.UUID;
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+    private UUID idExist;
+    private UUID idNoExist;
+    @BeforeEach
+    void setUp() throws Exception{
+        idExist = UUID.fromString("6fe825eb-c626-4b19-bac2-02dace9dff6c");
+        idNoExist =UUID.fromString("2ba8135c-973d-403b-b3f8-9a28a9e0f427");
+    }
+
+    @Test
+    public void shouldDeleteObjectWhenIdExists(){
+        UUID userbank = idExist;
+//        System.out.println(userbank);
+        userRepository.deleteById(userbank);
+        Optional<UserBank> result = userRepository.findById(userbank);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void shouldThrowEmptyResultDataAccessExceptionIfNotExists(){
+    Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+        userRepository.deleteById(idNoExist);
+    });
+    }
+
+    @Test
+    public void shouldReturnUserbankIfIdExists(){
+        Optional<UserBank> userBank = userRepository.findById(idExist);
+        Assertions.assertTrue(userBank.isPresent());
+    }
+
+    @Test
+    public void shouldReturnNullIfIdNoExists(){
+        Optional<UserBank> userBank = userRepository.findById(idNoExist);
+        Assertions.assertTrue(userBank.isEmpty());
+    }
+
+
 }
