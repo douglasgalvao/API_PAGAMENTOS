44a17b3eccab377f638983063e5bce60927c9c1a
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-11 15:32:25 -0300

    Refactoring to job test

---
+++ src/test/java/com/Payment_API/entities/UserBank.java
@@ -0,0 +1,8 @@
+package com.Payment_API.entities;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+public class User { }
